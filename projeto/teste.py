d2c24a836bdccc20e4065a0920d387497cab361f
gi <135059916+giovannalimang@users.noreply.github.com>
2024-10-13 18:36:47 -0300

    Update teste.py

--- projeto/teste.py
+++ projeto/teste.py
@@ -1,7 +1,10 @@
 from flask import Flask
-from controller import filme_controllers
+from controllers import filme_controllers
 
 app = Flask(__name__)
+
+app.secret_key = 'sua_chave_secreta_aqui' 
+
 app.register_blueprint(filme_controllers)
 
 if __name__ == "__main__":
