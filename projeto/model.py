1fd9536170f16723c2c58086b5df4a4101e9fb65
gi <135059916+giovannalimang@users.noreply.github.com>
2024-10-20 14:54:03 -0300

    Update model.py

--- projeto/model.py
+++ projeto/model.py
@@ -1,3 +1,4 @@
+
 class Filme:
     def __init__(self, id, descricao, completo=False):
         self.id = id
@@ -6,10 +7,22 @@ class Filme:
 
 listaFilmes = []
 
+usuarios = {
+    "user": "1234",
+    "admin": "5678"
+}
+
 def addFilme(descricao):
     id = len(listaFilmes) + 1  
     novo_filme = Filme(id, descricao)
     listaFilmes.append(novo_filme)
 
 def getFilmes():
-    return listaFilmes  
+    return listaFilmes
+
+def autenticar(user, senha):
+
+    if user in usuarios:
+        if usuarios[user] == senha:
+            return user
+    return None
