83b1300ac0b261654376e54b5afdbecf76957d01
gi <135059916+giovannalimang@users.noreply.github.com>
2024-10-20 14:53:28 -0300

    Update controller.py

--- projeto/controller.py
+++ projeto/controller.py
@@ -1,19 +1,29 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session, flash
-from models import Filme, addFilme, getFilmes
+from models import Filme, addFilme, getFilmes, autenticar
 
 filme_controllers = Blueprint("filme", __name__)
+
+@filme_controllers.before_request
+def before_request():
+    print(f"Método da Requisição: {request.method} | Caminho da Requisição: {request.path}")
+
+@filme_controllers.after_request
+def after_request(response):
+    response.set_cookie('visited', 'true')
+    return response
 
 @filme_controllers.route("/")
 def index():
     filmes = getFilmes()
-    username = session.get('username')  
-    return render_template('index.html', filmes=filmes, username=username)
+    username = session.get('username')
+    visited = request.cookies.get('visited')
+    return render_template('index.html', filmes=filmes, username=username, visited=visited)
 
 @filme_controllers.route("/add", methods=["POST"])
 def add():
     descricao = request.form["descricao"]
     addFilme(descricao)
-    return redirect(url_for('filme.index'))  
+    return redirect(url_for('filme.index'))
 
 @filme_controllers.route("/login", methods=['GET', 'POST'])
 def login():
@@ -21,18 +31,26 @@ def login():
         user = request.form['user']
         senha = request.form['senha']
         
-        session['username'] = user 
-        flash(f'Bem-vindo, {user}!', 'success') 
-        return redirect(url_for('filme.welcome'))  
-    return render_template('login.html')  
+        autenticado = autenticar(user, senha)
+        if autenticado:
+            session['username'] = user
+            flash(f'Bem-vindo, {autenticado}!', 'success')
+            return redirect(url_for('filme.welcome'))
+        else:
+            flash('Usuário ou senha inválidos.', 'danger')  
+    return render_template('login.html')
 
 @filme_controllers.route("/welcome")
 def welcome():
     username = session.get('username')
-    return render_template('welcome.html', username=username)
+    if username == "admin":
+        message = "Bem-vindo, ADMIN!"
+    else:
+        message = "Bem-vindo, USER!"
+    return render_template('welcome.html', username=username, message=message)
 
 @filme_controllers.route("/logout")
 def logout():
-    session.pop('username', None)  
-    flash('Você foi desconectado.', 'info')  
-    return redirect(url_for('filme.login'))  
+    session.pop('username', None)
+    flash('Você foi desconectado.', 'info')
+    return redirect(url_for('filme.login'))
