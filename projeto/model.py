5f2d82235e4ebc94175c11e83723bc61d3946fbc
gi <135059916+giovannalimang@users.noreply.github.com>
2024-11-01 23:57:28 -0300

    Update model.py

--- projeto/model.py
+++ projeto/model.py
@@ -21,8 +21,22 @@ def getFilmes():
     return listaFilmes
 
 def autenticar(user, senha):
-
     if user in usuarios:
         if usuarios[user] == senha:
             return user
     return None
+class FilmeAlugar:
+    def __init__(self, id, nome,imagem,preco):
+        self.id = id
+        self.nome = nome
+        self.preco = preco
+        self.imagem=imagem
+        
+
+listFilmesAlugar = []
+listFilmesAlugar.append(FilmeAlugar(1, "Gente Grande", 15.00, "gente_grande.jpg"))
+listFilmesAlugar.append(FilmeAlugar(2, "Avengers-Ultimato", 35.00, "avengers.jpg"))
+listFilmesAlugar.append(FilmeAlugar(3, "Enrolados", 10.00,"enrolados.jpg")),
+listFilmesAlugar.append(FilmeAlugar(4, "Todos menos você", 40.00,"todos.jpg")),
+listFilmesAlugar.append(FilmeAlugar(5, "Harry Potter4", 20.00,"harry.jpg")),
+listFilmesAlugar.append(FilmeAlugar(6, "It-A coisa", 25.00,"it.jpg"))
